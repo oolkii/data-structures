33e3686448656ad1593d59315e19bd1c11028c36
vivian.mzh@gmail.com <vivian.mzh@gmail.com>
2016-09-14 15:40:48 -0700

    finished passing test suite for queue.

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -5,14 +5,29 @@ var Queue = function() {
   var storage = {};
 
   // Implement the methods below
+  var counter = 0;
+  var firstPosition = 0;
+  var length = function() {
+    return counter - firstPosition;
+  }
 
   someInstance.enqueue = function(value) {
+    storage[counter] = value;
+    counter++;
   };
 
   someInstance.dequeue = function() {
+    if (firstPosition === counter) {
+      return;
+    }
+    var temp = storage[firstPosition];
+    delete storage[firstPosition];
+    firstPosition++;
+    return temp;
   };
 
   someInstance.size = function() {
+    return length();
   };
 
   return someInstance;
