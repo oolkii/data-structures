a8723427edb7b5de418dbcd070000d0532ece1a8
Oolkii <benjaminolki@gmail.com>
2016-09-15 15:11:10 -0700

    function instantiation for pseudoclassical stack updated to pass testing suite

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,26 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.counter = 0;
+  this.firstPosition = 0;
 };
 
+Queue.prototype.enqueue = function(value) {
+  this[this.counter] = value;
+  this.counter++;
+};
 
+Queue.prototype.dequeue = function() {
+  var temp = this[this.firstPosition];
+  delete this[this.firstPosition];
+  if (this.firstPosition < this.counter) {
+    this.firstPosition++;
+  }
+  return temp;
+};
+
+Queue.prototype.size = function() {
+  return this.counter - this.firstPosition;
+};
+
+var queue = new Queue();
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,20 @@
-var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+var Stack = function(start) {
+  this.counter = 0;
 };
 
-
+Stack.prototype.push = function(value) {
+  this[this.counter] = value;
+  this.counter++;
+};
+Stack.prototype.pop = function () {
+  if (this.counter > 0) {
+    this.counter--;
+  }
+  var temp = this[this.counter];
+  delete this[this.counter];
+  return temp;
+};
+Stack.prototype.size = function() {
+  return this.counter;
+};
+var stack = new Stack();
