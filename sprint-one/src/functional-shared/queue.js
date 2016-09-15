a5cd7230aaead1ce54a7bbb95225c62cfc9fe7ea
Oolkii <benjaminolki@gmail.com>
2016-09-14 22:30:44 -0700

    completed testing suite for stack -functional completed testing suite for queue -functional completed testing suite for stack -functional-shared completed testing suite for queue -functional-shared

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,39 @@
 var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+// define new object
+  var instance = {};
+// add private variables to new object
+  instance.counter = 0;
+// add queueMethods to new object
+  instance.firstPosition = 0;
+  instance.enqueue = queueMethods.enqueue;
+  instance.dequeue = queueMethods.dequeue;
+  instance.size = queueMethods.size;
+// return new object
+  return instance;
 };
 
 var queueMethods = {};
+
+
+//  create method for enqueue - pushes to end of line and adds to counter
+queueMethods.enqueue = function (value) {
+  this[this.counter] = value;
+  this.counter++;
+};
+//  create method for dequeue - pops the first position and moves first position up one -
+queueMethods.dequeue = function () {
+  var temp = this[this.firstPosition];
+  delete this[this.firstPosition];
+  if (this.firstPosition < this.counter) {
+    this.firstPosition++;
+  }
+  return temp;
+};
+//  create method for size that finds the length by evaluating difference between counter and first position
+queueMethods.size = function () {
+  return this.counter - this.firstPosition;
+};
+
+
 
 
