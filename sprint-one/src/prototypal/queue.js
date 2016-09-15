1328be046206aa5cd223cc6bebc81e045f049a18
Oolkii <benjaminolki@gmail.com>
2016-09-15 15:09:30 -0700

    function prototypal instantiation updated to pass testing suite

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,28 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var instance = Object.create(queueMethods);
+  instance.counter = 0;
+  instance.firstPosition = 0;
+  return instance;
 };
 
 var queueMethods = {};
 
+queueMethods.enqueue = function(value) {
+  this[this.counter] = value;
+  this.counter++;
+};
 
+queueMethods.dequeue = function() {
+  var temp = this[this.firstPosition];
+  delete this[this.firstPosition];
+  if (this.firstPosition < this.counter) {
+    this.firstPosition++;
+  }
+  return temp;
+};
+
+queueMethods.size = function() {
+  return this.counter - this.firstPosition;
+};
