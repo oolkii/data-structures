0c935621305f3290c62b6f9e77096136351f1b53
Oolkii <benjaminolki@gmail.com>
2016-09-15 10:59:52 -0700

    Testing suite passing for stack in prototypal pattern

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,30 @@
 var Stack = function() {
+  var instance = {};
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  instance.counter = 0;
+  instance.push = stackMethods.push;
+  instance.pop = stackMethods.pop;
+  instance.size = stackMethods.size;
+  return instance;
 };
 
 var stackMethods = {};
 
+stackMethods.push = function (value) {
+  this[this.counter] = value;
+  this.counter++;
+};
 
+stackMethods.pop = function () {
+  if ( this.counter > 0 ) {
+    this.counter--;
+  }
+  var temp = this[this.counter];
+  delete this[this.counter];
+  return temp;
+};
+
+stackMethods.size = function () {
+  return this.counter;
+};
