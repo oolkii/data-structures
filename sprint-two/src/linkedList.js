f5cf34d687c84b4348d83a1a6ce80e2e11e3cc71
Oolkii <benjaminolki@gmail.com>
2016-09-15 21:39:51 -0700

    linked list test suite fulfilled for removehead, contains and addtail

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -2,16 +2,44 @@ var LinkedList = function() {
   var list = {};
   list.head = null;
   list.tail = null;
+  list.length = 0;
 
   list.addToTail = function(value) {
+    var currentNode = this.head;
+    var newNode = new Node(value);
+
+    if (!currentNode) {
+      this.head = this.tail = newNode;
+      this.length++;
+      return newNode;
+    }
+    while (currentNode.next) {
+      currentNode = currentNode.next;
+    }
+    this.tail = currentNode.next = newNode;
+    this.length++;
   };
 
   list.removeHead = function() {
+    var temp = this.head;
+    delete this.head;
+    if (temp.next !== null) {
+      this.head = temp.next;
+    }
+    return temp.value;
   };
 
   list.contains = function(target) {
+    var flag = false;
+    var currentNode = this.head;
+    while (currentNode) {
+      if (currentNode.value === target) {
+        flag = true;
+      }
+      currentNode = currentNode.next;
+    }
+    return flag;
   };
-
   return list;
 };
 
