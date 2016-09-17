82c2c8015cbf8b022faa018a7547376a206fc306
Oolkii <benjaminolki@gmail.com>
2016-09-17 10:04:04 -0700

    test suite passed 100% for graph and tree data structures

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -1,39 +1,71 @@
 
-
 // Instantiate a new graph
 var Graph = function() {
+  this.nodes = {};
+  this.edges = {};
 };
 
 // Add a node to the graph, passing in the node's value.
-Graph.prototype.addNode = function(node) {
+Graph.prototype.addNode = function(value) {
+  this.nodes[value] = value;
 };
 
 // Return a boolean value indicating if the value passed to contains is represented in the graph.
-Graph.prototype.contains = function(node) {
+Graph.prototype.contains = function(value) {
+  if (this.nodes[value]) {
+    return this.nodes[value] === value; // same as if else statement
+  }
+  return false;
 };
 
 // Removes a node from the graph.
-Graph.prototype.removeNode = function(node) {
+Graph.prototype.removeNode = function(value) {
+  this.forEachNode(function(node) {
+    if (this.edges[node] === value) {
+      delete this.edges[node];
+    }
+  });
+  delete this.edges[value];
+  delete this.nodes[value];
 };
 
 // Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
 Graph.prototype.hasEdge = function(fromNode, toNode) {
+  if (this.edges[fromNode] && this.edges[toNode]) {
+    return this.edges[fromNode][toNode] === toNode && this.edges[toNode][fromNode] === fromNode;
+  }
+  return false;
 };
 
 // Connects two nodes in a graph by adding an edge between them.
 Graph.prototype.addEdge = function(fromNode, toNode) {
+  if (this.edges[toNode] === undefined) {
+    this.edges[toNode] = {};
+  }
+  if (this.edges[fromNode] === undefined) {
+    this.edges[fromNode] = {};
+  }
+  this.edges[fromNode][toNode] = toNode;
+  this.edges[toNode][fromNode] = fromNode;
 };
 
 // Remove an edge between any two specified (by value) nodes.
 Graph.prototype.removeEdge = function(fromNode, toNode) {
+  delete this.edges[fromNode][toNode];
+  delete this.edges[toNode][fromNode];
 };
 
 // Pass in a callback which will be executed on each node of the graph.
 Graph.prototype.forEachNode = function(cb) {
+  for (var key in this.nodes) {
+    console.log(this.nodes[key])
+    cb(this.nodes[key]);
+  }
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
  */
 
+var graph = Graph();
 
--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -3,18 +3,41 @@ var Tree = function(value) {
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];  // fix me
 
+  newTree.addChild = treeMethods.addChild;
+  newTree.contains = treeMethods.contains;
   return newTree;
 };
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+// check if tree has children
+  // if so, loop thru children, add each children to ..
+// loop thru each child, call .addChild on each child
+  this.children.push(Tree(value));
 };
 
 treeMethods.contains = function(target) {
+// set a result variable to false
+// loop thru tree's children
+  // if target equal any children, change result to true
+// return result
+  var result = false;
+  var recursiveSearch = function (tree) {
+    tree.children.forEach(function(node) {
+      if (node.value === target) {
+        result = true;
+      } else {
+        recursiveSearch(node);
+      }
+    });
+  };
+  recursiveSearch(this);
+  return result;
 };
+
 
 
 
