6e06914d6892e3589e1f46f7849ebd5596899dba
Oolkii <benjaminolki@gmail.com>
2016-09-17 17:19:51 -0700

    reimplemented hashtables to meet testing suite standard and without use of objects in structure design

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -4,67 +4,40 @@ var HashTable = function() {
   this._limit = 8;
   this._storage = LimitedArray(this._limit); // a dressing room
   this._encumberance = 0;
-  console.log(this._storage);
+
+
 };
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit); // special key converted for k(hash key)
-  var dresser = this._storage[index];
-  if ( dresser === undefined ) {
-    dresser = [];
-    this._storage[index] = dresser;
+  if (this._storage.get(index)) {
+    this._storage.set(index, [this._storage.get(index), [k, v]]);
+  } else {
+    this._storage.set(index, [k, v]);
   }
-  var overwrite = false;
-  dresser.forEach(function(drawer) {
-    if (drawer[0] === k) { // if k is found
-      drawer[1] = v;        // overwrite the v
-      overwrite = true;
-    }
-  });
-  if (!overwrite) {         // if k not found
-    dresser.push([k, v]);    // create k, v
-    this._encumberance++;
-    // if (this._encumberance === .75 * this._limit) {
-    //   console.log("resizing!");
-    //   temp = this._storage;
-    //   this._limit -= (.5 * this._limit);
-    //   this._storage = LimitedArray(this._limit);
-    //   for (var key in temp) {
-    //     //console.log(typeof temp, typeof key, temp);
-    //     key.forEach(function(drawer) {
-    //       this.insert(drawer[0], drawer[1]);
-    //     });
 
-    //   }
-    // }
-  }
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  var dresser = this._storage[index];
-  var content;
-  dresser.forEach(function(drawer) {
-    if (drawer[0] === k) {
-      content = drawer[1];
-    }
-  });
-  return content;
+  var tupleValue;
+  if (Array.isArray(this._storage.get(index)[0])) {
+    this._storage.get(index).forEach(function(hashTuple) {
+      if (hashTuple[0] === k) {
+        tupleValue = hashTuple[1];
+      }
+    });
+  } else {
+    tupleValue = this._storage.get(index)[1];
+  }
+  return tupleValue;
+
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  var dresser = this._storage[index];
-  if ( dresser === undefined ) {
-    dresser = [];
-    this._storage[index] = dresser;
-  }
-  for (var i = 0; i < dresser.length; i++) {
-    if (dresser[i][0] === k) {
-      dresser.splice(i, 1);
-    }
-    this._encumberance--;
-  }
+  this._storage.set(index, [undefined, undefined]);
+
   HashTable.prototype.resize = function (newLimit) {
     var temp = this._storage;
     var tempLim = this._limit;
@@ -72,19 +45,9 @@ HashTable.prototype.remove = function(k) {
 
 
 
-  };
-  // if (this._encumberance === .25 * this._limit) {
-  //   console.log("resizing!");
-  //   temp = this._storage;
-  //   this._limit -= (.5 * this._limit);
-  //   this._storage = LimitedArray(this._limit);
-  //   temp.forEach(function(dresser) {
-  //     dresser.forEach(function(drawer) {
-  //       this.insert(drawer[0], drawer[1]);
-  //     });
-  //   });
-  // }
 
+
+  };
 };
 
 
